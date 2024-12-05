30934a91fa69cadd8b99e65adcda3d0890c2bf02
chriskornaros <chriskornaros@gmail.com>
2024-12-05 08:44:11 -0600

    I get how uv works now (localized env, that uv is aware of, in the project folders, similar to git/dbt.

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
+import duckdb
+
 def main():
-    print("Hello from uv-basic!")
+    print("Hello from uv-basic!", "\n", duckdb.__version__) # This lets me test and make sure that the package is successfully installed
 
 
 if __name__ == "__main__":
